b1bbe78c4f6afc5cea58677ea94ed87eddfcd40f
Joaquin Barrientos <joaquin2899@gmail.com>
2024-03-23 22:52:17 -0600

    Updated feeder_gateway.go

--- internal/datasource/feeder_gateway.go
+++ internal/datasource/feeder_gateway.go
@@ -1,10 +1,15 @@
 package datasource
 
-import "context"
+import (
+	"context"
+
+	"github.com/charmbracelet/log"
+)
 
 type FeederGateway struct {
 }
 
 func (fg *FeederGateway) SyncBlock(ctx context.Context, block uint64) error {
+	log.Info("Sync", "block", block)
 	return nil
 }
